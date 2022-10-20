f757792700e21de7f296f6e2ed4e809ec7f7de51
Dwijen Chawra <dwchawra@gmail.com>
2022-10-20 01:33:50 -0400

    added tokens

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ url = "https://api.notion.com/v1/databases/todos-472716bb9fca4d05a32bdd20d65d187
 
 payload = {
     "page_size": 100,
-    "filter": {"and": [{
-                "property": "Done",
-                "checkbox": {"equals": True}
-            }, {"or": [
-                    {
-                        "property": "Tags",
-                        "contains": "A"
-                    },
-                    {
-                        "property": "Tags",
-                        "contains": "B"
-                    }
-                ]}]}
+    "filter": {
+        "and": [
+            {"property": "Completed", "checkbox": {"equals": False}}#,
+            # {
+            #     "or": [
+            #         {"property": "Tags", "contains": "A"},
+            #         {"property": "Tags", "contains": "B"},
+            #     ]
+            # },
+        ]
+    },
 }
 headers = {
     "accept": "application/json",
     "Notion-Version": "2022-06-28",
-    "content-type": "application/json"
+    "content-type": "application/json",
+    "authorization": ""
 }
 
 response = requests.post(url, json=payload, headers=headers)
