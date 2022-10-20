19034e53aef4ebcef2ef75f4e1dec50c0ed2cdfd
Dwijen Chawra <dwchawra@gmail.com>
2022-10-20 01:27:37 -0400

    added filters

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import requests
 
-url = "https://api.notion.com/v1/databases/database_id/query"
+url = "https://api.notion.com/v1/databases/todos-472716bb9fca4d05a32bdd20d65d187f/query"
 
-payload = {"page_size": 100}
+payload = {
+    "page_size": 100,
+    "filter": {"and": [{
+                "property": "Done",
+                "checkbox": {"equals": True}
+            }, {"or": [
+                    {
+                        "property": "Tags",
+                        "contains": "A"
+                    },
+                    {
+                        "property": "Tags",
+                        "contains": "B"
+                    }
+                ]}]}
+}
 headers = {
     "accept": "application/json",
     "Notion-Version": "2022-06-28",
