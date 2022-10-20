434d0b2fee41132ffbcf3a8ab6ff49a3c5e533e6
Dwijen Chawra <dwchawra@gmail.com>
2022-10-20 01:25:00 -0400

    barebones notion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import EventKit
+import requests
 
-store = EventKit
+url = "https://api.notion.com/v1/databases/database_id/query"
+
+payload = {"page_size": 100}
+headers = {
+    "accept": "application/json",
+    "Notion-Version": "2022-06-28",
+    "content-type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.text)
