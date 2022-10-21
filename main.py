3e93b8e208fcbdcb2acc34a39b9ecfb8ff9fbb96
Dwijen Chawra <dwchawra@gmail.com>
2022-10-21 00:17:26 -0400

    added argparse

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import os
 import requests
 from dotenv import load_dotenv
 import json
+import argparse
 
 load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--datatype", help="select from 'incomplete'")
+parser.add_argument("--datatype", help="", required=False)
+args = parser.parse_args()
 
 
 url = "https://api.notion.com/v1/databases/e19eff6dfb4745919abd6cfd66dc54f2/query"
@@ -12,7 +18,7 @@ payload = {
     "page_size": 100,
     "filter": {
         "and": [
-            {"property": "Completed", "checkbox": {"equals": False}}#,
+            {"property": "Completed", "checkbox": {"equals": False}}  # ,
             # {
             #     "or": [
             #         {"property": "Tags", "contains": "A"},
@@ -26,7 +32,7 @@ headers = {
     "accept": "application/json",
     "Notion-Version": "2022-06-28",
     "content-type": "application/json",
-    "authorization": os.getenv('TOKEN')
+    "authorization": os.getenv("TOKEN"),
 }
 
 response = requests.post(url, json=payload, headers=headers)
@@ -35,9 +41,5 @@ response = requests.post(url, json=payload, headers=headers)
 
 data = json.loads(response.text)
 
-item = data["results"][0]
+item = data["results"]
 print(item)
-
-json_formatted_str = json.dumps(item, indent=2)
-
-print(json_formatted_str)
