8b8fd09f6dcf1da0388e533a87d8601e6a10a733
Dwijen Chawra <dwchawra@gmail.com>
2022-12-01 03:04:12 -0500

    reverted back from ijson

--- main.py
+++ main.py
@@ -7,27 +7,44 @@ import argparse
 load_dotenv()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--datatype", help="select from 'incomplete'")
-parser.add_argument("--datatype", help="", required=False)
+parser.add_argument("--status", help="select from 'incomplete', 'complete'")
+parser.add_argument("--uuids", help="", required=False)
 args = parser.parse_args()
 
 
 url = "https://api.notion.com/v1/databases/e19eff6dfb4745919abd6cfd66dc54f2/query"
 
-payload = {
-    "page_size": 100,
-    "filter": {
-        "and": [
-            {"property": "Completed", "checkbox": {"equals": False}}  # ,
-            # {
-            #     "or": [
-            #         {"property": "Tags", "contains": "A"},
-            #         {"property": "Tags", "contains": "B"},
-            #     ]
-            # },
-        ]
-    },
-}
+
+if args.status == "complete":
+    payload = {
+        "page_size": 100,
+        "filter": {
+            "and": [
+                {"property": "Completed", "checkbox": {"equals": True}}  # ,
+                # {
+                #     "or": [
+                #         {"property": "Tags", "contains": "A"},
+                #         {"property": "Tags", "contains": "B"},
+                #     ]
+                # },
+            ]
+        },
+    }
+else:
+    payload = {
+        "page_size": 100,
+        "filter": {
+            "and": [
+                {"property": "Completed", "checkbox": {"equals": False}}  # ,
+                # {
+                #     "or": [
+                #         {"property": "Tags", "contains": "A"},
+                #         {"property": "Tags", "contains": "B"},
+                #     ]
+                # },
+            ]
+        },
+    }
 headers = {
     "accept": "application/json",
     "Notion-Version": "2022-06-28",
